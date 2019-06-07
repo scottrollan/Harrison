c99327b3aef193beed2a88c0c731c25b71a742dc
Barry Scott Rollan <barry.rollan@gmail.com>
2019-06-07 09:46:43 -0400

    here goes nuthin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from './components/HomePage';
 import Services from './components/Services';
@@ -9,14 +9,16 @@ import Pricing from './components/Pricing';
 
 function App() {
   return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
           <div className="App">
+            <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/home" component={HomePage} />
             <Route exact path="/services" component={Services} />
             <Route exact path="/contact" component={Contact} />
             <Route exact path="/aboutMe" component={AboutMe} />
             <Route exact path="/pricing" component={Pricing} />
+            </Switch>
           </div>
         </Router>
   );
